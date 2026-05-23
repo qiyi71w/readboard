f045c27feeb455ac181a57e07ff7f7d0be4b9ba2
石炜 <14329649+shi-wei-hello@user.noreply.gitee.com>
2026-05-24 02:03:00 +0800

    fix: avoid hanging pending move placement

--- tests/Readboard.VerificationTests/Protocol/YikeBackgroundPlaceTests.cs
+++ tests/Readboard.VerificationTests/Protocol/YikeBackgroundPlaceTests.cs
@@ -554,9 +554,10 @@ namespace Readboard.VerificationTests.Protocol
                 return true;
             }
 
-            public void SendMouseMessage(IntPtr handle, uint message, int wParam, int lParam)
+            public bool TrySendMouseMessage(IntPtr handle, uint message, int wParam, int lParam)
             {
                 SentMessages.Add(new MouseMessage(handle, message, wParam, lParam));
+                return true;
             }
         }
 
